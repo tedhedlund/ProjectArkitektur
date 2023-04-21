140bdb3977466eca410778bc186cf571e58cee2d
JHC1133 <joarhyltencavallius@gmail.com>
2023-04-21 13:04:35 +0200

    Update Test.cs
    Testar att göra ändringar i ett script

--- ProjectArkitektur/Assets/Scripts/Test.cs
+++ ProjectArkitektur/Assets/Scripts/Test.cs
@@ -4,10 +4,14 @@ using UnityEngine;
 
 public class Test : MonoBehaviour
 {
+
+    string test;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        test = "Denna string är ett test";
     }
 
     // Update is called once per frame
