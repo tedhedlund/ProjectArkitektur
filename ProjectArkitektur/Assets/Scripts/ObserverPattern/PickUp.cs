f103dc5b9b3cf436d88dafcef98bf7d3ff843ef4
Winkler99 <wilmerwinkler@live.se>
2023-05-09 16:49:44 +0200

    PickUp

--- ProjectArkitektur/Assets/Scripts/ObserverPattern/PickUp.cs
+++ ProjectArkitektur/Assets/Scripts/ObserverPattern/PickUp.cs
@@ -6,14 +6,44 @@ public class PickUp : MonoBehaviour
 {
     [SerializeField] private IntEventSO ammoEvent;
     [SerializeField] private int ammoValue = 30;
+    [SerializeField] private GameObject player;
+    [SerializeField] private Vector3 distance;
 
-    private void OnTriggerEnter(Collider other)
+    private void Start()
     {
-        if (other.CompareTag("Player"))
+        
+    }
+
+    //private void OnTriggerEnter(Transform other)
+    //{
+    //    if (1 >= Vector3.Distance(other.transform.position, collider.transform.position)) 
+    //    {
+    //        Debug.Log("Colliding with Player");
+    //        ammoEvent.Invoke(ammoValue);
+    //        Destroy(gameObject);
+    //    }
+
+        
+    //}
+
+    private void Update()
+    {
+        if (2 >= Vector3.Distance(gameObject.transform.position, player.transform.position))
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                Debug.Log("E pressed");
+                ammoEvent.Invoke(ammoValue);
+                Destroy(gameObject);
+            }
+        }
+        distance = gameObject.transform.position - player.transform.position;
+        if ( Vector3.Distance(player.transform.position, gameObject.transform.position) <= 2)
         {
             Debug.Log("Colliding with Player");
             ammoEvent.Invoke(ammoValue);
             Destroy(gameObject);
         }
+
     }
 }
