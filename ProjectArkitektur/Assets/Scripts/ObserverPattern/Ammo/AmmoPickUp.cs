8918c06f390bcd81976e10d421cd15282abc45e3
JHC1133 <joarhyltencavallius@gmail.com>
2023-05-23 23:59:46 +0200

    Rensat upp lite i eventklasserna

--- ProjectArkitektur/Assets/Scripts/ObserverPattern/Ammo/AmmoPickUp.cs
+++ ProjectArkitektur/Assets/Scripts/ObserverPattern/Ammo/AmmoPickUp.cs
@@ -4,26 +4,12 @@ using UnityEngine;
 
 public class AmmoPickUp : MonoBehaviour
 {
-    [SerializeField] private IntEventSO ammoEvent;
-    [SerializeField] private int ammoValue = 30;
+    [SerializeField] private EventSO ammoEvent;
     [SerializeField] private Vector3 distance;
 
     private GameObject player;
     private GameObject ammoBox;
 
-    // pickUp.AmmoSpawn(agent.transform.position); //Sätta in i ammoManager?
-
-    //private void OnTriggerEnter(Transform other)
-    //{
-    //    if (1 >= Vector3.Distance(other.transform.position, collider.transform.position)) 
-    //    {
-    //        Debug.Log("Colliding with Player");
-    //        ammoEvent.Invoke(ammoValue);
-    //        Destroy(gameObject);
-    //    }
-
-
-    //}
 
     void Start()
     {
@@ -37,17 +23,8 @@ public class AmmoPickUp : MonoBehaviour
         if ( Vector3.Distance(player.transform.position, gameObject.transform.position) <= 2)
         {
             Debug.Log("Colliding with Player");
-            //ammoEvent.InvokeAmmo(ammoValue);
             ammoEvent.InvokeAmmo();                 // Sending an invoke to all subscribers
             Destroy(gameObject);
         }
-
-    }
-
-    public void AmmoSpawn(Vector3 position)
-    {
-        Quaternion rotation = new Quaternion(0, 0, 0, 0);
-
-        Instantiate(ammoBox, position, rotation);
     }
 }
--- ProjectArkitektur/Assets/Scripts/ObserverPattern/Ammo/EventSO.cs
+++ ProjectArkitektur/Assets/Scripts/ObserverPattern/Ammo/EventSO.cs
@@ -4,16 +4,10 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [CreateAssetMenu]
-
-public class IntEventSO : ScriptableObject
+public class EventSO : ScriptableObject
 {
-    public event Action<int> EventAmmo;
-
     public delegate void SetMaxAmmo();
     public SetMaxAmmo maxAmmo;
-    
-
-    public void InvokeAmmo(int value) => EventAmmo?.Invoke(value);
 
     // Check if there is any subscribers, if there are we invoke
     public void InvokeAmmo() => maxAmmo?.Invoke();
